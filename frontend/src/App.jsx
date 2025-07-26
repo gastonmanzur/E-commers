b52a7fd15187830d1549acec9fedb0d0faf2afe9
gastonmanzur <57376378+gastonmanzur@users.noreply.github.com>
2025-07-26 14:06:48 -0300

    Fix App component to import React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Register from './pages/Register.jsx';
 import Login from './pages/Login.jsx';
 import Profile from './pages/Profile.jsx';
