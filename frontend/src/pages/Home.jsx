d9beab756389e89a390f246cdacbe40e4edf7a29
gastonmanzur <57376378+gastonmanzur@users.noreply.github.com>
2025-07-30 10:17:53 -0300

    Remove navbar-carousel spacing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="container-fluid p-0 mt-3">
+      <div className="container-fluid p-0">
         <div
           id="homeCarousel"
           className="carousel slide"
