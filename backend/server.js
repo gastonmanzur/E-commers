df4e3c29acaf37673a12c046660843714d21a850
gastonmanzur <57376378+gastonmanzur@users.noreply.github.com>
2026-01-15 23:53:21 -0300

    Clean up Mongo connection config

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,8 @@ app.get('/', (req, res) => {
   res.send('API Running');
 });
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
 app.use('/api/users', authRoutes);
